0fc19f232b099f08741437d6e6bae7890f90c514
d2omi1ng3o7 <dawidmucha@elektronikonline.pl>
2022-10-12 21:55:11 +0200

    23

--- Chess/main.py
+++ Chess/main.py
@@ -4,17 +4,16 @@ import os
 class Chess():
     def __init__(self):
         self.chess_board = const.CHESS_BOARD
+        self.whose_move = 'w'
 
     def printChessBoard(self):
-        print()
-        print('---------------------------------------------------------')
+        print('\n', '---------------------------------------------------------', sep='')
         counter = 1
         for i in self.chess_board:
             print(self.chess_board[i], end='', sep='')
             if counter == 8:
                 print('|', end='')
-                print()
-                print('---------------------------------------------------------')
+                print('\n', '---------------------------------------------------------', sep='')
                 counter = 1
             else:
                 counter += 1
@@ -28,8 +27,18 @@ class Chess():
 
     def move(self):
         move = input()
-        piece = self.chess_board[move[:2]]
+        figure = self.chess_board[move[:2]]
+
+        if figure[3:4] != self.whose_move:
+            return
+
+        if figure[3:4] == self.chess_board[move[2:4]][3:4]:
+            return
+
         self.chess_board[move[:2]] = '|      '
-        self.chess_board[move[2:4]] = piece
+        self.chess_board[move[2:4]] = figure
 
-
+        if self.whose_move == 'w':
+            self.whose_move = 'b'
+        else:
+            self.whose_move = 'w'
