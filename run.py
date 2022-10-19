1ec0fa74bbefdf3afe67a977298bafca31c4fca5
d2omi1ng3o7 <dawidmucha@elektronikonline.pl>
2022-10-19 19:36:52 +0200

    move rook and change files structures

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from Chess.main import Chess
+from src.main import Chess
 
 if __name__ == '__main__':
     game = Chess()
---
+++ src/Rook.py
@@ -0,0 +1,40 @@
+from constants import CHESS_BOARD
+
+# generujesz listę ruchem i potem sprawdzasz czy użytkownik podał jakiś ruch z tej listy
+
+class Rook:
+    def __init__(self):
+        self.listWithMoves = []
+        self.moveAbovePieces = False
+    
+
+    def moveList(self, position='45'): # Do poprawy: dodaj możliwość wybrania dowolnej pozycji, połącz w jedno te same pętle linie i kolumny
+
+        for x in range(int(position[:1])-1, 1, -1):
+            new_position = f'{x}5'
+            if not(self.moveAbovePieces) and '  ' != CHESS_BOARD[new_position]:
+                break
+            self.listWithMoves.append(new_position)
+
+        for x in range(int(position[:1])+1, 9):
+            new_position = f'{x}5'
+            if not(self.moveAbovePieces) and '  ' != CHESS_BOARD[new_position]:
+                break
+            self.listWithMoves.append(new_position)
+
+        for x in range(int(position[1:])-1, 1, -1):
+            new_position = f'4{x}'
+            if not(self.moveAbovePieces) and '  ' != CHESS_BOARD[new_position]:
+                break
+            self.listWithMoves.append(new_position)
+
+        for x in range(int(position[1:])+1, 9):
+            new_position = f'4{x}'
+            if not(self.moveAbovePieces) and '  ' != CHESS_BOARD[new_position]:
+                break
+            self.listWithMoves.append(new_position)
+
+obj = Rook()
+
+obj.moveList()
+print(obj.listWithMoves)
