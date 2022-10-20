2d35afa23271218fd0fbd5e4d4adad3166b8af11
d2omi1ng3o7 <dawidmucha@elektronikonline.pl>
2022-10-20 18:48:53 +0200

    udpate bishop.py

--- src/bishop.py
+++ src/bishop.py
@@ -1,7 +1,7 @@
 from constants import CHESS_BOARD
 
 
-class Rook:
+class Bishop:
     def __init__(self):
         pass
 
@@ -9,32 +9,32 @@ class Rook:
 
         listWithMoves = []
 
-        for x in range(int(position[:1])-1, 1, -1):
-            new_position = f'{x}{position[1:]}'
+        for x in range(1, max(8-int(position[:1]), int(position[1:])-1)+1):
+            new_position = f'{str(int(position[:1])-x)}{str(int(position[1:])+x)}'
             if '  ' != CHESS_BOARD[new_position]:
                 if color != CHESS_BOARD[new_position][:1]:
                     listWithMoves.append(new_position)
                 break
             listWithMoves.append(new_position)
 
-        for x in range(int(position[:1])+1, 9):
-            new_position = f'{x}{position[1:]}'
+        for x in range(1, max(8-int(position[:1]), 8-int(position[1:]))+1):
+            new_position = f'{str(int(position[:1])+x)}{str(int(position[1:])+x)}'
             if '  ' != CHESS_BOARD[new_position]:
                 if color != CHESS_BOARD[new_position][:1]:
                     listWithMoves.append(new_position)
                 break
             listWithMoves.append(new_position)
 
-        for x in range(int(position[1:])-1, 1, -1):
-            new_position = f'{position[:1]}{x}'
+        for x in range(1, max(int(position[:1])-1, 8-int(position[1:]))+1):
+            new_position = f'{str(int(position[:1])+x)}{str(int(position[1:])-x)}'
             if '  ' != CHESS_BOARD[new_position]:
                 if color != CHESS_BOARD[new_position][:1]:
                     listWithMoves.append(new_position)
                 break
             listWithMoves.append(new_position)
 
-        for x in range(int(position[1:])+1, 9):
-            new_position = f'{position[1:]}{x}'
+        for x in range(1, max(int(position[:1])-1, int(position[1:])-1)+1):
+            new_position = f'{str(int(position[:1])-x)}{str(int(position[1:])-x)}'
             if '  ' != CHESS_BOARD[new_position]:
                 if color != CHESS_BOARD[new_position][:1]:
                     listWithMoves.append(new_position)
@@ -42,7 +42,3 @@ class Rook:
             listWithMoves.append(new_position)
 
         return listWithMoves
-
-obj = Rook()
-
-obj.moveList()
